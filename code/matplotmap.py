fe1294093fdcb8328243841b04f5e7c02af99418
viegas <guilherme.viegas@aquare.la>
2020-12-30 22:30:00 -0300

    update

--- code/matplotmap.py
+++ code/matplotmap.py
@@ -1,3 +1,9 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+df = pd.read_csv('data/bc_imoveis.csv')
+
+BBox = ((df.lng.min(),   df.lng.max(), df.lat.min(), df.lat.max()))
+
+ruh_m = plt.imread('img/map.osm')
